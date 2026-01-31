24bd7d74e2d2ea2e0b9bd44185d41c4dd7788174
Allen Yi <yiwenyou_allen@outlook.com>
2026-01-31 15:38:54 +0800

    zsh: add dotfiles bin to PATH

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,6 +1,7 @@
 # File: env.zsh - Environment variables shared across shells.
 # Environment defaults.
-export PATH="$HOME/.local/bin:$PATH"
+DOTFILES_DIR="${${(%):-%N}:A:h:h}"
+export PATH="$DOTFILES_DIR/bin:$HOME/.local/bin:$PATH"
 export EDITOR="nvim"
 export LANG="zh_CN.UTF-8"
 
