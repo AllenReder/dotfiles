d1e42ab257cfbb52e81458761f1681925a034e8e
Wenyou Yi <yiwenyou_allen@outlook.com>
2026-02-07 14:38:38 +0800

    add clash.sh

---
+++ clash.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git clone --branch master --depth 1 https://gh-proxy.org/https://github.com/nelvko/clash-for-linux-install.git \
+  && cd clash-for-linux-install \
+  && bash install.sh
